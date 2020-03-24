dd0675107743b60bf396a022d6bebd5cec9ef006
Ataeddin-kh <m.ataeddin.kh@gmail.com>
2020-03-24 14:28:51 +0430

    on_delet problem solved

--- web/models.py
+++ web/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Expence(models.Model):
-    text = models.CharField()
+    text = models.CharField(max_length=255)
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User,on_delete=models.DO_NOTHING)
     date = models.DateTimeField()
