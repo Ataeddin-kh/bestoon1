79cc5fb212f4813e8263285c4d2ad078af07d0bf
Ataeddin-kh <m.ataeddin.kh@gmail.com>
2020-04-03 00:08:06 +0430

    change in models

--- web/models.py
+++ web/models.py
@@ -2,10 +2,11 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-class token(models.Model):
+class Token(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     token =models.CharField(max_length=48)
-
+    def __str__(self):
+        return "{}-token".format(self.user)
 class Expence(models.Model):
     text = models.CharField(max_length=255)
     amount = models.BigIntegerField()
--- web/views.py
+++ web/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from web.models import Token,Expence,Income,User
+from datetime import datetime
 # Create your views here.
-"""user submit an  expense"""
+@csrf_exempt
+
+
 def submit_expense(request):
+    now=datetime.now()
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token = this_token ).get()
+    Expence.objects.create(text=request.POST['text'],user=this_user
+    ,amount=request.POST['amount'],date=now)
+    """user submit an  expense"""
     print("we are here")
+    print(request.POST)
+    return JsonResponse({
+    "status":"ok"},
+    encoder=JSONEncoder)
