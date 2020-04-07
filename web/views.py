ebbdbf251a4ab16eea527548131aa635600892de
Ataeddin-kh <m.ataeddin.kh@gmail.com>
2020-04-08 01:02:44 +0430

    add submit_income to views and urls

--- web/views.py
+++ web/views.py
@@ -7,16 +7,30 @@ from datetime import datetime
 # Create your views here.
 @csrf_exempt
 
-
+#TODO;user and amount and text might be fake
 def submit_expense(request):
-    now=datetime.now()
+    if 'date' in request.POST:
+        now=request.POST['date']
+    else :
+        now=datetime.now()
     this_token = request.POST['token']
     this_user = User.objects.filter(token__token = this_token ).get()
     Expence.objects.create(text=request.POST['text'],user=this_user
     ,amount=request.POST['amount'],date=now)
-    """user submit an  expense"""
-    print("we are here")
-    print(request.POST)
+    return JsonResponse({
+    "status":"ok"},
+    encoder=JSONEncoder)
+
+@csrf_exempt
+def submit_income(request):
+    if 'date' in request.POST:
+        now=request.POST['date']
+    else :
+        now=datetime.now()
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token = this_token ).get()
+    Income.objects.create(text=request.POST['text'],user=this_user
+    ,amount=request.POST['amount'],date=now)
     return JsonResponse({
     "status":"ok"},
     encoder=JSONEncoder)
